6c58a9151798ba0e858de18e0a9a6391399ede1c
saurabhkaipurkar <123494114+saurabhkaipurkar@users.noreply.github.com>
2024-10-17 16:46:00 +0530

    git version update

--- app/src/main/java/com/example/ShishuGyan/Home.java
+++ app/src/main/java/com/example/ShishuGyan/Home.java
@@ -7,6 +7,9 @@ import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class Home extends AppCompatActivity {
     LinearLayout layout,table,story,drawing,games,wiki,calci;
@@ -15,6 +18,11 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         layout=findViewById(R.id.layout_1);
         table=findViewById(R.id.table_layout);
